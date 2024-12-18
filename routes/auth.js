9362a81e5808c6b5cde1d7e66e15fdee5e16f74d
Anikethanshetty <shettyanikethand@gmail.com>
2024-12-18 22:37:58 +0530

    added rate limiter

--- routes/auth.js
+++ routes/auth.js
@@ -3,12 +3,28 @@ const bcrypt = require('bcryptjs');
 const User = require('../models/User');  // Adjust path if necessary
 const {zodLoginverify} = require("../middlewares/userDetailszod");
 const {userjwtverify} = require("../middlewares/userJWt")
+import { rateLimit } from 'express-rate-limit'
 const router = express.Router();
 
+const otplimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, 
+    max: 5, 
+    standardHeaders: 'draft-8', 
+    legacyHeaders: false, 
+    message: 'Too many OTP requests from this IP, please try again after 15 minutes.',
+});
+
+const passwordLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, 
+    max: 10, 
+    standardHeaders: 'draft-8', 
+    legacyHeaders: false, 
+    message: 'Too man requests from this IP, please try again after 15 minutes.',
+});
 
 router.post('/register', zodLoginverify, require("../controllers/register"));
-router.get("/verifyEmail",require("../controllers/verifyEmail")); 
-router.post('/signin', require("../controllers/signin") );
+router.get("/verifyEmail",otplimiter,require("../controllers/verifyEmail")); 
+router.post('/signin',passwordLimiter, require("../controllers/signin") );
 router.get("/forgotPassword",require("../controllers/forgotPassword"))
 router.post("/resetPasswordOtp",require("../controllers/resetPasswordOtp"))
 
